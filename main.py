9af181e1646158447fa555451216b786e96a3a3f
aindriu18 <aindriuinglis@gmail.com>
2023-03-20 17:51:30 +0000

    adding main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+What does a machine learning expert do?
+
+#1 - they will import the data, usually a csv file.
+#2 - they clean the data. All columns and rows are filled correctly as sometimes cells and rows are missing data.
+#3 - they split the data into a training and test set. Training set is for machine to learn. Test set is to
+     tell it if it was right or wrong.
+#4 - create a model. Usually they import an algorithm and use a library.
+#5 - check the output of the model with test set.
+#6 - improve. Most likely it will improve after each iteration
+
+"""
+
+
